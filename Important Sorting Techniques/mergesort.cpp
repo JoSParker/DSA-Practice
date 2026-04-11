6dcac31d6c2db3a7e6926372010099d6383a9142
John Samuel P <jsamuelp181@gmail.com>
2026-04-11 19:08:41 +0530

    d

---
+++ Important Sorting Techniques/mergesort.cpp
@@ -0,0 +1,9 @@
+void mergesort(vector<int> &a , int low, int high){
+           int mid=(low+high)/2;
+           ms(a,low,mid);
+           ms(a,mid+1;high);
+           
+}
+void merge(vector<int> &a, int low,int mid, int high){
+          
+}
